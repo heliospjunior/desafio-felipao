993d7b5b6ae3ec232a5cb333d80ff848f6dacda1
heliospjunior <heliospjunior@gmail.com>
2023-12-25 19:19:58 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,21 @@
+const nome = "Helio";
+const xp = 2505;
+
+
+    if (xp < 1000) {
+    console.log("O Herói de nome " + nome + "está no nível de Ferro");
+     } else if (1001 < xp < 2000){
+    console.log("O Herói de nome " + nome + "está no nível de Bronze");
+    } else if (2001 < xp < 5000) {
+    console.log("O Herói de nome " + nome + "está no nível de Prata");
+    } else if (5001 < xp < 7000){
+    console.log("O Herói de nome " + nome + "está no nível de Ouro");
+    } else if (7001 < xp < 8000){
+    console.log("O Herói de nome " + nome + " está no nível de Platina");
+    } else if (8001 < xp < 9000){
+    console.log("O Herói de nome " + nome + "está no nível de Ascedente");
+    } else if (9001 < xp < 10000){
+    console.log("O Herói de nome " + nome + "está no nível de Imortal");
+    }else if (xp > 10000){
+        console.log("O Herói de nome " + nome + "está no nível de Radiante");
+    };
